249844c4b661d2cae42117bf9697ea32f3477ac8
Nurusatto <nursat21102006@gmail.com>
2025-12-04 22:04:26 +0500

    fix page

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -10,20 +10,22 @@ export const metadata: Metadata = {
   description: "Book page",
 };
 
-export default async function Book({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const res = await fetch(`https://openlibrary.org/works/${id}.json`);
-  if (!res.ok) return notFound();
-  const book = await res.json();
+export default async function Book(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+
+  let book;
+  try {
+    const res = await fetch(`https://openlibrary.org/works/${id}.json`);
+    if (!res.ok) return notFound();
+    book = await res.json();
+  } catch (e) {
+    console.error(e);
+    return notFound();
+  }
 
   return (
-    <>
+    <HistoryProvider book={book}>
       <BookPage bookObj={book} />
-      <HistoryProvider book={book} />
-    </>
+    </HistoryProvider>
   );
 }
--- src/providers/historyProvider.tsx
+++ src/providers/historyProvider.tsx
@@ -2,9 +2,16 @@
 
 import { BookInf } from "@/entities/book";
 import { useBookHistory } from "@/store/useBookHistory";
+import { ReactNode } from "react";
 
-export const HistoryProvider = ({ book }: { book: BookInf }) => {
+export const HistoryProvider = ({
+  book,
+  children,
+}: {
+  book: BookInf;
+  children: ReactNode;
+}) => {
   const addHistory = useBookHistory((book) => book.setHistory);
   addHistory(book);
-  return null;
+  return <>{children}</>;
 };
